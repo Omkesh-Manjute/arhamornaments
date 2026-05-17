0245eb6227e428f6622cf3385ff53ff06c2c7b16
Omkesh-Manjute <omkeshmanjute1@gmail.com>
2026-05-17 06:51:20 +0530

    fix(mobile): remove deprecated setDomStorageEnabled from webview setup

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -50,7 +50,6 @@ class _WebAppScreenState extends State<WebAppScreen> {
     
     controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setDomStorageEnabled(true) // Crucial to prevent React LocalStorage crashes!
       ..setUserAgent("ArhamOrnamentsWebView")
       ..setBackgroundColor(const Color(0x00000000))
       ..setNavigationDelegate(
